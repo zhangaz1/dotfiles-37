b97f8c4bdf6a1a6bf3f10c893d2f3bb23b1266fa
Kamil Zabielski <kamil.zabielski@sysdogs.com>
2019-09-20 13:50:15 +0200

    Add spacefish
    Signed-off-by: Kamil Zabielski <kamil.zabielski@sysdogs.com>

--- dotfiles-fish/fish/config.fish
+++ dotfiles-fish/fish/config.fish
@@ -5,12 +5,6 @@ set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin/" $fish_user_pat
 ## }}}1
 
 
-## fisher-pure-prompt {{{1
-set -U pure_threshold_command_duration 2
-set -U pure_begin_prompt_with_current_directory true
-## }}}1
-
-
 ## fisher-z {{{1
 set -U FZF_FIND_FILE_COMMAND "fd --type f . \$dir"
 set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:3 --inline-info --height 80%'
@@ -19,4 +13,25 @@ set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:3 --inline-info --height 80
 
 ## fisher-fzf {{{1
 set -U Z_CMD "z"
+## }}}1
+
+
+## fisher-spacefish-prompt {{{1
+set -U SPACEFISH_USER_SHOW false
+set -U SPACEFISH_NODE_SHOW false
+set -U SPACEFISH_JULIA_SHOW false
+set -U SPACEFISH_RUBY_SHOW false
+set -U SPACEFISH_HASKELL_SHOW false
+set -U SPACEFISH_CONDA_SHOW false
+set -U SPACEFISH_PHP_SHOW false
+set -U SPACEFISH_RUST_SHOW false
+set -U SPACEFISH_DOTNET_SHOW false
+set -U SPACEFISH_PACKAGE_SHOW false
+
+set -U SPACEFISH_KUBECONTEXT_SHOW false
+set -U SPACEFISH_KUBECONTEXT_NAMESPACE_SHOW false
+
+set -U SPACEFISH_BATTERY_THRESHOLD 100
+
+set -U SPACEFISH_PROMPT_SEPARATE_LINE false
 ## }}}1
