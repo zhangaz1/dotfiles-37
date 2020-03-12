f1af76ceb0bc71b60f7b7857742aa9182e2fad7e
Kamil Zabielski <kamil.zabielski@sysdogs.com>
2020-03-12 12:17:34 +0000

    Add emacs
    Signed-off-by: Kamil Zabielski <kamil.zabielski@sysdogs.com>

---
+++ src/dotfiles-emacs/emacs.d/init.el
@@ -0,0 +1,9 @@
+(custom-set-variables)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(global-font-lock-mode 1)
